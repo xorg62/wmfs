c8e82eac86b42f242cc7f94efa84dff88f41a8cb
Marc Lagrange <marc@shebang.sangor>
2009-01-07 15:40:13 +0100

    python/setup.py: Fix build on FreeBSD, edit name/desc/etc...
    Signed-off-by: Marc Lagrange <marc@shebang.sangor>

--- python/setup.py
+++ python/setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('wmfs',
-                    #include_dirs = ['/usr/local/include'],
+                    include_dirs = ['/usr/local/include'],
                     libraries = ['X11'],
-                    #library_dirs = ['/usr/local/lib'],
+                    library_dirs = ['/usr/local/lib'],
                     sources = ['screen_func.c',
                                'tag_func.c',
                                'libwmfs.c'])
 
 setup (name = 'wmfs',
        version = '0.1',
-       description = 'desc',
-       author = 'author',
-       author_email = 'mail',
+       description = 'WMFS client library',
+       author = 'Martin Duquesnoy',
+       author_email = '',
        url = 'http://wmfs.sangor.net/',
        long_description = '''
-desc long
+This is a control librarie for the WM named WMFS.
 ''',
        ext_modules = [module1])
 
