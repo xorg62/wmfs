4cc91384858c4958c9149dbd6c26d939c4ac655c
Martin Duquesnoy <xorg62@gmail.com>
2009-04-23 18:03:25 +0200

    Scan: Add XFree(ret) for each XGetWindowProperty()

--- src/wmfs.c
+++ src/wmfs.c
@@ -206,11 +206,17 @@ scan(void)
                {
                     if(XGetWindowProperty(dpy, w[i], ATOM("_WMFS_TAG"), 0, 32,
                                           False, XA_CARDINAL, &rt, &rf, &ir, &il, &ret) == Success && ret)
+                    {
                          tag = *ret;
+                         XFree(ret);
+                    }
 
                     if(XGetWindowProperty(dpy, w[i], ATOM("_WMFS_SCREEN"), 0, 32,
                                           False, XA_CARDINAL, &rt, &rf, &ir, &il, &ret) == Success && ret)
+                    {
                          screen = *ret;
+                         XFree(ret);
+                    }
 
                     c = client_manage(w[i], &wa);
 
@@ -221,9 +227,6 @@ scan(void)
 
                     arrange(c->screen);
                }
-
-     if(ret)
-          XFree(ret);
 
      XFree(w);
      arrange(screen_get_sel());
