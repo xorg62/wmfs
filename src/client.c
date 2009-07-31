33a7864da2523f29ddbeac19bda20a62652ee4a1
Martin Duquesnoy <xorg62@gmail.com>
2009-08-01 01:20:08 +0200

    Client: Add condition in uicb_client_resize

--- src/client.c
+++ src/client.c
@@ -1043,10 +1043,10 @@ uicb_client_resize(uicb_t cmd)
 
      if(sscanf(cmd, "%d %d", &wi, &hi))
      {
-          geo.width += wi;
-          geo.height += hi;
+          geo.width += ((geo.width + wi > 0) ? wi : 0);
+          geo.height += ((geo.height + hi > 0) ? hi : 0);
 
-          client_moveresize(sel, geo, False);
+          client_moveresize(sel, geo, True);
      }
 
      return;
