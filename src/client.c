7180d733bd98e2a5d92f3793f370d3b2957c0768
Raphael Khaiat <raphael@khaiat.org>
2011-02-16 19:20:58 +0100

    Correct previous feature for raising tiled client

--- src/client.c
+++ src/client.c
@@ -1190,8 +1190,8 @@ client_update_attributes(Client *c)
 void
 client_raise(Client *c)
 {
-     if((!c || ((c->flags & TileFlag) && !(c->flags & AboveFlag)))
-        && !conf.client_tile_raise)
+     if(!c || !(c->flags & AboveFlag) 
+           || ((c->flags & TileFlag) && !conf.client_tile_raise))
           return;
 
      XRaiseWindow(dpy, c->frame);
