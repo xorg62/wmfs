97fd00885662eb59d3847fe6fef77aa2459c11b5
Martin Duquesnoy <xorg62@gmail.com>
2012-01-22 01:49:25 +0100

    Add default mousebind for client (scroll to focus next/prev tab) and minor fix

--- src/client.c
+++ src/client.c
@@ -407,7 +407,7 @@ client_frame_update(struct client *c, struct colpair *cp)
                     barwin_resize(c->titlebar, f, c->tbarw);
 
                     barwin_refresh_color(c->titlebar);
-                    draw_rect(cc->titlebar->dr, g, c->scol.bg);
+                    draw_rect(c->titlebar->dr, g, c->scol.bg);
                     draw_text(c->titlebar->dr, c->theme, xt, y, cp->fg, c->title);
                     barwin_refresh(c->titlebar);
 
