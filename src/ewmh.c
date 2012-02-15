7dd2bcaec9786bf9b35a939ab297ddae12af101f
Martin Duquesnoy <xorg62@gmail.com>
2012-02-15 19:24:52 +0100

    Fix hole in layout when removing fullscreen state

--- src/ewmh.c
+++ src/ewmh.c
@@ -220,7 +220,7 @@ ewmh_manage_state(long data[], struct client *c)
                                (unsigned char*)0, 0);
                XReparentWindow(W->dpy, c->win, c->frame, c->wgeo.x, c->wgeo.y);
 
-               layout_client(c);
+               layout_fix_hole(c);
           }
      }
 }
